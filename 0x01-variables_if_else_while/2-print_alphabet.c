5de09190ecbaae2b1abf99a649bc8396279c0ea6
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-01 02:35:22 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+
+	return (0);
+}
