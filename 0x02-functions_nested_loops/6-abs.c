ee93ff97878c276db238ba5d4007f548b2eb7dde
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-05 00:04:47 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @i: The integer to compute with
+ *
+ * Return: The absolute value of i
+ */
+int _abs(int i)
+{
+	return (i < 0 ? i * -1 : i);
+}
