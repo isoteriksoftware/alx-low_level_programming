5a28598e1921dd341e31bdb50c1f2aecf993cdd2
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-07 02:42:48 -0700

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	long num = 612852475143;
+
+	for (c = (int) sqrt(num); c > 2; c++)
+	{
+		if (num % c == 0)
+		{
+			printf("%d\n", c);
+			break;
+		}
+	}
+
+	return (0);
+}
