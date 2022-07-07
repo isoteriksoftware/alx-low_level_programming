4f6051599e70bb8a37123669df85207af68e7625
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-07 02:39:22 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle, followed by a new line
+ * @size: The size of the triangle
+ */
+void print_triangle(int size)
+{
+	int c, i, j;
+
+	c = 0;
+	i = size - 1;
+	while (c < size)
+	{
+		i = size - 1 - c;
+		j = c + 1;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		while (j > 0)
+		{
+			_putchar('#');
+			j--;
+		}
+		_putchar('\n');
+		c++;
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
