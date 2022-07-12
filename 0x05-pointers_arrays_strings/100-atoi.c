7e06233f8a037139bc352f53fd14ad5fb74b3309
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-12 02:04:52 -0700

    task 10 fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,7 +13,7 @@ int _atoi(char *s)
 
 	i = 0;
 	value = 0;
-	sign = 1;
+	sign = -1;
 	while (s[i] != '\0')
 	{
 		c = s[i];
@@ -21,7 +21,10 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (c >= '0' && c <= '9')
 		{
-			value = value * 10 + (c - '0');
+			if (value < 0)
+				value = value * 10 - (c - '0');
+			else
+				value = (c - '0') * -1;
 
 			/* Make sure the next char is a number */
 			if (s[i + 1] < '0' || s[i + 1] > '9')
