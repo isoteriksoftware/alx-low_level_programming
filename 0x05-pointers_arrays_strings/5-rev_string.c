8a04e2a0a78025b6fb5842426e93087c8bc15747
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-12 00:05:19 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i, j;
+	char temp;
+
+	/* Get the length */
+	while (s[len])
+		len++;
+
+	/* Swap each character */
+	i = len;
+	while (--i > len / 2)
+	{
+		j = len - i - 1;
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+
+	}
+}
