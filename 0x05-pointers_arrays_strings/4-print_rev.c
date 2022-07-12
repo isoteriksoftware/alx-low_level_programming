82348de35cbca899de33abacf5f76c77991d4b4d
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-11 23:24:45 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse, followed by a new line
+ * @s: The string
+ */
+void print_rev(char *s)
+{
+	/* Figure out the length first */
+	int len = 0;
+
+	while (s[len])
+		len++;
+
+	/* Print backwards */
+	while (len > 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
