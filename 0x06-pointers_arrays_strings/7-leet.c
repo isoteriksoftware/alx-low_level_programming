a40a69526d4cd880d62a5fdd2b0be6464098b115
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-14 04:30:20 -0700

    task 7 fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,16 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char subs[] = "aAeEoOtTlL";
+	char le[] = "43071";
 
+	i = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; subs[j] != '\0'; j++)
+			if (s[i] == subs[j])
+				s[i] = le[j / 2];
 	}
 
 	return (s);
