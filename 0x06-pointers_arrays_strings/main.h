60c4b7dbe539cec005556f54948a11ed154666d2
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-14 04:17:49 -0700

    task 11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,3 +9,5 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
