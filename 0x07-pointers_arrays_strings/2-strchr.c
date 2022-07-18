cb59a3c2d79859879e9fe5cbb4fc18ea665f3bfe
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-18 05:47:32 -0700

    task 2 fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+		s++;
 	}
 
-	return (NULL);
+	return (s + 1);
 }
