2752cdf6dd3bb9ce100d1d238577456b6287f678
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-20 00:27:28 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,11 +3,13 @@
 /**
  * _strlen_recursion - returns the length of a string.
  * @s: the string
+ *
+ * Return: the length of the string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
-	
-	return _strlen_recursion(++s) + 1;
+		return (0);
+
+	return (_strlen_recursion(++s) + 1);
 }
