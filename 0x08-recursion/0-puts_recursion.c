f6173d7b15e5e3ea055c81ad893365eca07db114
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-19 23:49:12 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * prints a string, followed by a new line.
+ * @s: the string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(++s);
+}
