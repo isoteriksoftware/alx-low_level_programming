91ba3255e73053639478ce69f8dd60ed203493f5
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-19 23:51:33 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * prints a string, followed by a new line.
+ * _puts_recursion - prints a string, followed by a new line.
  * @s: the string
  */
 void _puts_recursion(char *s)
