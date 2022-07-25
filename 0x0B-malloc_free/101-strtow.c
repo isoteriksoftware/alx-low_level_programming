65c8332f35faac9abced0e90a51cb84e893da673
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-07-25 05:01:48 -0700

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <stdlib.h>
+
+void util(char **words, char *str);
+
+/**
+ * strtow - splits a string into words.
+ * @str: the string
+ *
+ * Return: returns a pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+	int i, len = 0;
+	char **words;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ' && str[i + 1] != '\0'
+			       && str[i + 1] != ' ')
+			len++;
+		i++;
+	}
+
+	words = (char **)malloc(sizeof(char *) * (len + 1));
+	if (words == NULL)
+		return (NULL);
+
+	util(words, str);
+	words[len] = NULL;
+	return (words);
+}
+
+/**
+ * util - a util function for fetching words into an array
+ * @words: the strings array
+ * @str: the string
+ */
+void util(char **words, char *str)
+{
+	int i, j, k, l, start, end;
+
+	i = k = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ' && str[i + 1] != '\0'
+			       && str[i + 1] != ' ')
+		{
+			j = 0;
+			i++;
+			start = i;
+
+			while (str[i] != ' ' && str[i] != '\0')
+			{
+				j++;
+				i++;
+			}
+
+			end = start + j;
+			words[k] = (char *)malloc(sizeof(char) * (j + 1));
+
+			for (l = 0; start < end; start++, l++)
+				words[k][l] = str[start];
+			words[k][l] = '\0';
+			k++;
+		}
+		else
+			i++;
+	}
+}
