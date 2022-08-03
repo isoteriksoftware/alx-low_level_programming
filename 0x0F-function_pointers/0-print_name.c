5b1ec6ca8a19b6957ed478c64f478879b60150c9
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-08-03 12:16:19 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: the name
+ * @f: the function to print with
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
