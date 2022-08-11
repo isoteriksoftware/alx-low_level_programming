ec298c81091ee97353676dd8bf2710ab7ee03e8c
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-08-11 13:41:53 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			continue;
+		}
+
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
